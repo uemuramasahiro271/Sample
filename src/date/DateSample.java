f04570729503999d83bf2c9f45e0454d8de385a5
uemuramasahiro271 <uemura.free2020@gmail.com>
2020-05-25 21:06:07 +0900

    Mavenプロジェクトに変換

---
+++ src/date/DateSample.java
@@ -0,0 +1,37 @@
+package date;
+
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class DateSample {
+
+	public void execute() {
+
+		// 現在日時取得
+		Date date = new Date();
+		System.out.println(date);
+
+        LocalDateTime ldt1 = LocalDateTime.now();
+        System.out.println(ldt1);
+
+        // フォーマット指定
+        var pattern = "yyyy年MM月dd日E曜日k時mm分ss秒";
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        System.out.println(format.format(date));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        System.out.println(ldt1.format(formatter));
+
+        // LocalDateTimeのparseは時刻を指定しないとエラーになる
+        LocalDateTime ldt2 = LocalDateTime.parse("2018/05/07 10:00:00", DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        System.out.println(ldt2);
+
+        LocalDateTime ldt3 = LocalDateTime.of(2000, 5, 8, 10, 22);
+        System.out.println(ldt3);
+        System.out.println(ldt3.with(DayOfWeek.SATURDAY));
+        System.out.println(ldt3.with(DayOfWeek.SATURDAY).isBefore(ldt3));
+        System.out.println(ldt3.with(DayOfWeek.SATURDAY).isAfter(ldt3));
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,16 @@
 package main;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import anonymous.AnonymousSample;
 import collection.CollectionSample;
-import date.DateSample;
 import reflection.ReflectionSample;
 
 public class Main extends JFrame {
@@ -22,30 +24,27 @@ public class Main extends JFrame {
 
 	private Main(String title) {
 
-		var sample = new DateSample();
-		sample.execute();
+		setTitle(title);
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-//		setTitle(title);
-//		setSize(300, 200);
-//		setLocationRelativeTo(null);
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//		JPanel panel = new JPanel();
-//
-//		JButton button1 = createJButton("リフレクション");
-//		setButtonClickAction(button1, reflectionListener());
-//		panel.add(button1);
-//
-//		JButton button2 = createJButton("匿名クラス");
-//		setButtonClickAction(button2, anonymousListener());
-//		panel.add(button2);
-//
-//		JButton button3 = createJButton("コレクション");
-//		setButtonClickAction(button3, collectionListener());
-//		panel.add(button3);
-//
-//	    Container contentPane = getContentPane();
-//	    contentPane.add(panel, BorderLayout.CENTER);
+		JPanel panel = new JPanel();
+
+		JButton button1 = createJButton("リフレクション");
+		setButtonClickAction(button1, reflectionListener());
+		panel.add(button1);
+
+		JButton button2 = createJButton("匿名クラス");
+		setButtonClickAction(button2, anonymousListener());
+		panel.add(button2);
+
+		JButton button3 = createJButton("コレクション");
+		setButtonClickAction(button3, collectionListener());
+		panel.add(button3);
+
+	    Container contentPane = getContentPane();
+	    contentPane.add(panel, BorderLayout.CENTER);
 	}
 
 	private JButton createJButton(String text) {
